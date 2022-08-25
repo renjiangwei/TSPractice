a72845700cdac6b17c29ebf7858e98a386503286
renjiangwei <1223995087@qq.com>
2022-08-25 14:41:31 +0800

    fix:泛型补充

--- base/src/test/14_template.ts
+++ base/src/test/14_template.ts
@@ -65,9 +65,50 @@ type h = g<f>
     type g = 'a' extends 'a' | 'b'  ? number : string
     type h<T> = T extends 'a' ? T : never
     type i = h<'a' | 'b'>
-    //    'a' extends 'a' ? 'a' : never  | 'b' extends 'a' ? b : never
+    // 'a' extends 'a' ? 'a' : never  | 'b' extends 'a' ? b : never
     // 'a' | never
     type j = 'a' | never | never | never
   }
+}
+{
+  // 接口泛型1
+  interface DF<T> {
+    a: T,
+    b: T[]
+  }
+  let a:DF<string> = {
+    a: '',
+    b: ['sad']
+  }
+  
+}
+{
+  // 函数泛型1
+  let a: <T>(arg: number, b: T)=>T 
+  a = <T>(b: number, c: T) => {
+    b
+    return c
+  }
+  a<string>(1, 'fd')
+  // 函数泛型2
+  interface Q {
+    <T>(a: T): T
+  }
+  let b: Q = <T>(ac: T) => {
+    return ac
+  }
+  b<number>(1)
+}
+{
+  // 类泛型
+  class S<T> {
+    static num: number
+    public a: T
+    constructor (a: T) {
+      S.num = 1
+      this.a = a
+    }
+  }
+  let a: S<string> = new S<string>('123')
 }
 export {}
